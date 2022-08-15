4c75b0c9419f0e40347f36c618c5a711c821d24b
fatihhhh <fatihisiteknik42@gmail.com>
2022-08-15 19:03:11 +0300

    Java gıtgub

--- src/day01_variables/C01_Variables.java
+++ src/day01_variables/C01_Variables.java
@@ -5,6 +5,7 @@ public class C01_Variables {
     public static void main(String[] args) {
         System.out.printf("Hello Java, Hello World");
         // JAva cok guzel
+        // Bugün gitgub ı da ogrendim
         
     }
 }
